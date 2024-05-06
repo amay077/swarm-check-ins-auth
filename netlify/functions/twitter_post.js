b830932c1aa6b5ff46c8e21339e869fc17a4902f
amay077 <okuokuoku@gmail.com>
2024-05-06 18:10:33 +0900

    twitter refresh_token

--- netlify/functions/twitter_post.js
+++ netlify/functions/twitter_post.js
@@ -3,12 +3,31 @@ const fetch = require('node-fetch')
 const handler = async (event) => {
   
   try {
-    const { access_token, text } = JSON.parse(event.body); // as { access_token: string, text: string };
+    const { refresh_token, text } = JSON.parse(event.body); // as { refresh_token: string, text: string };
+
+    const tokens = await (async () => {
+      const res = await fetch(`https://api.twitter.com/2/oauth2/token`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': `Basic ${process.env.TWITTER_CLIENT_BASIC_AUTH}`,        
+        },
+        body: `refresh_token=${refresh_token}&grant_type=refresh_token&client_id=${process.env.TWITTER_CLIENT_ID}`,
+      });
+      
+      if (!res.ok) {
+        console.error(`token request failed: ${res.status}`)
+        throw new Error(`token request failed: ${res.status}`);
+      }
+
+      const resJson = await res.json();
+      return resJson;
+    })();
 
     const res = await fetch(`https://api.twitter.com/2/tweets`, {
       method: 'POST',
       headers: {
-        'Authorization': `Bearer ${access_token}`,
+        'Authorization': `Bearer ${tokens.access_token}`,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ text }),
@@ -25,7 +44,8 @@ const handler = async (event) => {
       statusCode: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
-      }
+      },
+      body: JSON.stringify({ refresh_token: tokens.refresh_token })
     }
   } catch (error) {
     console.log(`handler -> error:`, error);
